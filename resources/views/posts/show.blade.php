be213600359cf3f6ec43d7bcbd1af60f20158286
shin133334 <shin133334@gmail.com>
2022-12-14 00:27:06 +0900

    tmp

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,22 +2,23 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Posts</title>
+        <title>Blog</title>
+        
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        
     </head>
-    <body>
+    <body class="antialiased">
         <h1 class="title">
             {{ $post->title }}
         </h1>
         <div class="content">
             <div class="content__post">
                 <h3>本文</h3>
-                <p>{{ $post->body }}</p>    
+                <p class="body">{{ $post->id }}</p>    
             </div>
         </div>
-        <div class="footer">
+        <div class="edit">
             <a href="/posts/{{ $post->id }}/edit">edit</a>
         </div>
         <div class="footer">
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,3 +20,4 @@ Route::get('/posts/create', [PostController::class,'create']);
 Route::get('/posts/{post}', [PostController::class, 'show']);
 Route::post('/posts', [PostController::class, 'store']);
 Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
+Route::get('/posts/{post}', [PostController::class, 'update']);
