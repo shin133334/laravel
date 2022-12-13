66dc627c5d8921283d9815d84353aa42cbd9776b
shin133334 <shin133334@gmail.com>
2022-12-13 17:49:13 +0900

    cal18

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,17 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 use App\Models\Post;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts/index')->with(['posts' => $post->getByLimit()]);
+        return view('posts/index')->with(['posts' => $post->getByLimit(1)]);
+    }
+    
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
     }
     
     public function create()
     {
         return view('posts/create');
+    }
+    
+    public function store(PostRequest $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
     }
 }
