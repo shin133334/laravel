3851ab672b5e407abcd81ee5213adc4ccd480bae
shin133334 <shin133334@gmail.com>
2022-12-14 17:16:06 +0900

    tmp

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -14,8 +14,24 @@
                 <div class='post'>
                    <a href="/posts/{{ $post->id }}"><h2 class='title'>{{ $post->title }}></h2></a>
                     <p class='body'>{{ $post->body }}</p>
+                    <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button type="button" onclick="deletePost({{ $post->id }})">delete</button>
+                    </form>
                 </div>
             @endforeach
         </div>
+        <script>
+            function deletePost(id)
+            {
+                'use strict'
+                
+                if(confirm('削除すると復元できません．\n 本当に削除しますか？'))
+                {
+                    document.getElementById(`form_${id}`).submit();
+                }
+            }
+        </script>
     </body>
 </html>
